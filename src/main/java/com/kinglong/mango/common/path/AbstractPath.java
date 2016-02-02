550bbe7bf885c1477e83b240666321e1e94f989e
jinlong.chen <jinlong.chen@tqmall.com>
2016-02-02 21:55:43 +0800

    bugfix

--- src/main/java/com/kinglong/mango/common/path/AbstractPath.java
+++ src/main/java/com/kinglong/mango/common/path/AbstractPath.java
@@ -15,6 +15,15 @@ public abstract class AbstractPath implements Path {
         this.value = value.trim();
     }
 
+    /**
+     * 设置一个路径有以下情况:
+     * (1).如果parent为空的话，那么该节点的路径为value值；
+     * (2).如果parent不为空的话，那么有如下情况:
+     *      1).如果parent为"/"的话,那么该节点为/{value}
+     *      2).如果parent不为"/"的话,那么该节点为{parent}/{value}/
+     * @param parent 父节点路径
+     * @param value parent节下子节点的名字
+     */
     public void value(Path parent, String value) {
         value = value.trim();
         if (parent == null) {
---
+++ src/main/java/com/kinglong/mango/exception/MangoNullZKClientException.java
@@ -0,0 +1,7 @@
+package com.kinglong.mango.exception;
+
+/**
+ * Created by chenjinlong on 16/1/31.
+ */
+public class MangoNullZKClientException {
+}
